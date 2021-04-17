b8329d902fad488dcf088810cde35a66492cf732
yoonsoyoung <32251962+yoonsoyoung@users.noreply.github.com>
2021-04-18 03:27:32 +0900

    Add files via upload

---
+++ baekjoon/단어뒤집기2_17413.java
@@ -0,0 +1,31 @@
+package com.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+//미완
+public class 단어뒤집기2_17413 {
+
+	static String s, nonRev, Rev;
+	static int cnt; //글자수
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		s = br.readLine();
+		Rev = "";
+		
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == '<') {
+				
+			}
+			
+			
+		}
+
+	}
+
+}

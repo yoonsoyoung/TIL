f2b86e324c3eae09a6e249e640f530dc87f326a7
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2022-01-10 23:55:55 +0900

    Comment beakjoon 촌수계산_2644 : Update 코드 설명 주석

--- baekjoon/촌수계산_2644.java
+++ baekjoon/촌수계산_2644.java
@@ -44,18 +44,18 @@ public class boj_2644_Main {
 			System.out.println(-1);
 	}
 
-	private static void bfs(int curr) {
+	private static void bfs(int start) {
 		Queue<Integer> q = new LinkedList<Integer>();
 		
-		q.add(curr);
-		visited[curr] = 1;
+		q.add(start); // 시작 정점 큐에 넣고
+		visited[start] = 1; // 해당 정점 방문 처리 = 촌수 1 (이때 나를 포함해버리기 때문에 결과에서는 1 빼줘야 함)
 		
 		while (!q.isEmpty()) {
-			int c = q.poll();
-			for (int i = 1; i <= n; i++) {
-				if(adjM[c][i] && visited[i] == 0) {
-					q.offer(i);
-					visited[i] = visited[c]+1;
+			int curr = q.poll(); // 탐색 하려는 정점 꺼냄
+			for (int i = 1; i <= n; i++) { // 인접 정점 탐색
+				if(adjM[curr][i] && visited[i] == 0) { // 탐색하려는 부모의 인접 정점이 존재하고 방문하지 않은 곳
+					q.offer(i); // 인접 정점 탐색 큐에 넣고
+					visited[i] = visited[curr]+1; // 현재 정점은 부모가 가진 촌수 + 1
 				}
 			}
 			

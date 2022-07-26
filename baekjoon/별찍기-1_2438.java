06299e96ad24ee2c1911c8d27d50ebf2f243e1f3
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2022-07-26 10:17:48 +0900

    Update 별찍기-1_2438 풀이방식 추가

--- baekjoon/별찍기-1_2438.java
+++ baekjoon/별찍기-1_2438.java
@@ -7,12 +7,19 @@ public class Main_2438 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		T = Integer.parseInt(br.readLine());
+		// 통과 212ms
+//		for (int i = 0; i < T; i++) {
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		// 통과 132ms
+		String res = "";
 		for (int i = 0; i < T; i++) {
-			for (int j = 0; j <= i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+			res += "*";
+			System.out.println(res);
 		}
 	}
-
 }

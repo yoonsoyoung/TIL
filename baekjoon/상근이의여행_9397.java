b10bca9570927166b466c610281a0287643b6e4b
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2023-02-17 15:34:33 +0900

    230217

---
+++ baekjoon/상근이의여행_9397.java
@@ -0,0 +1,33 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main_9372 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken()); // 국가의 수
+			int M = Integer.parseInt(st.nextToken()); // 비행기의 종류
+
+			for (int m = 0; m < M; m++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+			} // end m for
+
+
+			System.out.println(N-1);
+
+		} // end for t
+
+	}
+
+}

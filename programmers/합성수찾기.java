dca818bffe619ab5797ac0b949f1b4a132fd2f22
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2024-11-22 01:25:46 +0900

    241122

---
+++ programmers/합성수찾기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+// 통과
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i <= n; i++) {
+            for(int j = 2; j <= Math.sqrt(i) ; j++) {
+                if(i % j == 0) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}

992215486ee050d90e45fa3d6e1fbd6e9633a975
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2024-12-05 23:15:44 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.25 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++) {
+            answer++;
+            if(answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                i--;
+            }
+        }
+        return answer;
+    }
+}

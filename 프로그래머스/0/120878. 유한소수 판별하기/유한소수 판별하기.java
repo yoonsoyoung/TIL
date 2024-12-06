ae278267a6d500c8dbc93a226d98e8c6a139c2c4
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2024-12-06 17:34:33 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,32 +1,22 @@
 import java.util.*;
 class Solution {
     public int solution(int a, int b) {
-        int answer = 1;
-        int gcd = getGcd(a, b); // 최대공약수
-        a /= gcd;
-        b /= gcd;
+        b /= getGcd(a, b);
         
-        for(int i = 2; i <= 5; i+=3) { // 유한소수 판별 2, 5로 소인수 분해
-            while(b % i == 0) {
-                b /= i;
-            }
+        // 유한소수 판별 2, 5로 소인수 분해
+        while(b % 2 == 0) {
+            b /= 2;
+        }
+        while(b % 5 == 0) {
+            b /= 5;
         }
         
-        if(b != 1) answer = 2; // 위 소인수 분해 결과 몫이 1이 아니라면 무한소수
-        return answer;
+        // 위 소인수 분해 결과 몫이 1이 아니라면 무한소수
+        return b == 1 ? 1 : 2;
     }
     
+    // 유클리드 호제법 재귀
     public static int getGcd(int a, int b) {
-        int remain = 1;
-        int max = a > b ? a : b;
-        int min = a < b ? a : b;
-        while(true) {
-            remain = max % min;
-            if(remain == 0) {
-                return min;
-            }
-            max = min;
-            min = remain;
-        }
+        return b == 0 ? a : getGcd(b, a % b);
     }
 }

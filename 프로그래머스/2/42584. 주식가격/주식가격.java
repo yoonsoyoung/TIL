bd4603c279042c07e9833c35f0ffccfc1837697b
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2025-01-07 23:24:06 +0900

    [level 2] Title: 주식가격, Time: 14.85 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length - 1; i++) {
+            int cnt = 1;
+            for(int j = i + 1; j < prices.length - 1; j++) {
+                if(prices[i] > prices[j]) {
+                    break;
+                }
+                cnt++;
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+}

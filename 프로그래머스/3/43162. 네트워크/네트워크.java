4d9be553a62056a90413a8ea13b3f48e92b942ec
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2025-01-20 00:02:51 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    static boolean[] visited;
+    static ArrayList<Integer>[] adjList;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        adjList = new ArrayList[n];
+        for(int i = 0; i < n; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        
+        for(int i = 0; i < computers.length; i++) {
+            for(int j = 0; j < computers[i].length; j++) {
+                if(computers[i][j] == 1) {
+                    adjList[i].add(j);
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                answer = dfs(i, 0) > 0 ? answer : answer + 1;
+            }
+        }
+    
+        return answer;
+    }
+    
+    private static int dfs(int curr, int cnt) {
+        visited[curr] = true;
+        for(int next : adjList[curr]) {
+            if(!visited[next]) {
+                dfs(next, cnt + 1);
+            }
+        }
+        return cnt;
+    }
+}

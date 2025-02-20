3e8938129e9538731fbc67a1fc45bbf4d5cea2c0
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2025-02-20 21:30:28 +0900

    [level 2] Title: 피로도, Time: 34.67 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,25 @@
+function solution(k, dungeons) {
+    var answer = -1;
+    let visited = Array(dungeons.length).fill(false);
+    let result = [];
+    
+    function dfs(idx, cnt) {
+        answer = Math.max(answer, cnt);
+        for(let i = 0; i < dungeons.length; i++) {
+            if(!visited[i] && (k >= dungeons[i][0])) {
+                visited[i] = true;
+                k -= dungeons[i][1];
+                
+                result.push(cnt + 1);
+                dfs(idx + 1, cnt + 1);
+                
+                // 복구
+                visited[i] = false;
+                k += dungeons[i][1];
+            }
+        }
+    }
+    
+    dfs(0, 0);
+    return answer;
+}

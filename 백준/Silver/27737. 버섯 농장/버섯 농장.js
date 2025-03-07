accfc1c3339635668e61fef512f0a406090bcfff
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2025-03-08 03:25:12 +0900

    [Silver I] Title: 버섯 농장, Time: 164 ms, Memory: 11392 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27737. 버섯 농장/버섯 농장.js
+++ 백준/Silver/27737. 버섯 농장/버섯 농장.js
@@ -3,8 +3,15 @@ let N = input[0][0]; // map 크기
 let M = input[0][1]; // 포자 수
 let K = input[0][2]; // 연결 수
 let map = input.slice(1);
-let area = Array(N * N + 1).fill(0); // 인접한 구역별 칸 수 관리
+let area = Array(Math.floor(N * N / 2) + 2).fill(0); // 인접한 구역별 칸 수 관리
+let visited = Array.from({length: N}, () => Array(N).fill(false));
 
+class Spore {
+    constructor(r, c) {
+        this.r = r;
+        this.c = c;
+    }
+}
 const dr = [-1, 1, 0, 0];
 const dc = [0, 0, -1, 1];
 
@@ -20,18 +27,18 @@ map.forEach((row, i) => {
 let cnt = 0;
 map.forEach((row, i) => {
     row.forEach((v, j) => {
-        if(v === 0) {
-            dfs(i, j, ++cnt);
+        if(v === 0 && !visited[i][j]) {
+            bfs(i, j, ++cnt);
         }
     });
 });
 
 let needs = 0; // 필요한 포자 수
-area.forEach((v, i) => {
-    if(v > 0) {
-        needs += (area[i] % K === 0 ? area[i] / K : area[i] / K + 1);
+for(let i = 1; i < area.length; i++) {
+    if(area[i] > 0) {
+        needs += (area[i] % K === 0 ? Math.floor(area[i] / K) : Math.floor(area[i] / K) + 1);
     }
-});
+}
 
 if(cnt > 0 && M >= needs) {
     // cnt > 0: 최소 하나라도 포자를 사용해야 함 조건 체크
@@ -42,18 +49,28 @@ if(cnt > 0 && M >= needs) {
     console.log('IMPOSSIBLE');
 }
 
-function dfs(r, c, idx) {
-    map[r][c] = idx; // map 원본에 구역 인덱스 표기
-    area[idx]++; // 해당 구역 인덱스 칸 수 증가
+function bfs(r, c, idx) {
+    let q = [];
+    q.push(new Spore(r, c));
+    visited[r][c] = true;
 
-    for(let d = 0; d < 4; d++) {
-        let nr = r + dr[d];
-        let nc = c + dc[d];
+    while(q.length > 0) {
+        let curr = q.shift();
+        let cr = curr.r;
+        let cc = curr.c;
+        map[cr][cc] = idx;
+        area[idx]++;
 
-        if(nr < 0 || nc < 0 || nr >= N || nc >= N)
-            continue;
-        if(map[nr][nc] === 0) { // 0이 아닌 구역은 불가지역이거나 탐색된(인덱스 표기)구역
-            dfs(nr, nc, idx);
+        for(let d = 0; d < 4; d++) {
+            let nr = cr + dr[d];
+            let nc = cc + dc[d];
+
+            if(nr < 0 || nc < 0 || nr >= N || nc >= N)
+                continue;
+            if(!visited[nr][nc] && map[nr][nc] === 0) { // 0이 아닌 구역은 불가지역이거나 탐색된(인덱스 표기)구역
+                q.push(new Spore(nr, nc));
+                visited[nr][nc] = true;
+            }
         }
     }
 }

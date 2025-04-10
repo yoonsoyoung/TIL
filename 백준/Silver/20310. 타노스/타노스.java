4f6beaaafbc8f04e184071e5135686fd531d5a28
Humming_SY <32251962+yoonsoyoung@users.noreply.github.com>
2025-04-10 23:31:06 +0900

    [Silver III] Title: 타노스, Time: 136 ms, Memory: 16384 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -9,13 +9,31 @@ public class Main {
 		String s = br.readLine();
 		long cnt_zero = s.chars().filter(c -> c == '0').count();
 		long cnt_one = s.chars().filter(c -> c == '1').count();
+		int mid_zero = (int) cnt_zero / 2; 
+		int mid_one = (int) cnt_one / 2; 
 		
-		char[] charArr = s.toCharArray();
-		Arrays.sort(charArr);
-		String str = new String(charArr);
-		int mid_zero = (int) (cnt_zero % 2 == 0 ? cnt_zero / 2 : cnt_zero / 2 + 1); 
-		int mid_one = (int) (cnt_one % 2 == 0 ? cnt_one / 2 : cnt_one / 2 + 1); 
-		System.out.println(str.substring(mid_zero, str.length() - mid_one));
+		char[] cArr = s.toCharArray();
+		for(int i = 0; i < cArr.length; i++) { // 앞에서부터 '1' 절반 제거
+			if(cArr[i] == '1' && mid_one > 0) {
+				cArr[i] = ' ';
+				mid_one--;
+			}
+		}
+		for(int i = cArr.length - 1; i > 0; i--) { // 뒤에서부터 '0' 절반 제거
+			if(cArr[i] == '0' && mid_zero > 0) {
+				cArr[i] = ' ';
+				mid_zero--;
+			}
+		}
+		
+		String ns = "";
+		for(char c : cArr) {
+			if(c != ' ') {
+				ns += c;
+			}
+		}
+		
+		System.out.println(ns);
 
 	}
 
